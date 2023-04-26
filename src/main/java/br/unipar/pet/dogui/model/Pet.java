1e91b98a9c2ec73ba8feb85d3fa7eef46b3917b2
BrunoVian <bruno.vian@edu.unipar.br>
2023-04-26 19:25:16 -0300

    ajustes

--- src/main/java/br/unipar/pet/dogui/model/Pet.java
+++ src/main/java/br/unipar/pet/dogui/model/Pet.java
@@ -19,5 +19,55 @@ public class Pet {
     private GeneroEnum genero;
     private double peso;
     private PorteEnum porte;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Pessoa getDono() {
+        return dono;
+    }
+
+    public void setDono(Pessoa dono) {
+        this.dono = dono;
+    }
+
+    public GeneroEnum getGenero() {
+        return genero;
+    }
+
+    public void setGenero(GeneroEnum genero) {
+        this.genero = genero;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public PorteEnum getPorte() {
+        return porte;
+    }
+
+    public void setPorte(PorteEnum porte) {
+        this.porte = porte;
+    }
+    
+    
     
 }
